5426e4c5c5fa24a3e832c5ea8dc02858b36528b2
Rajan Maurya <rajanmaurya154@gmail.com>
2016-10-30 14:57:11 +0530

    Step 1: Create a Java library Completed

--- app/src/main/java/opensource/builditbigger/MainActivity.java
+++ app/src/main/java/opensource/builditbigger/MainActivity.java
@@ -1,13 +1,35 @@
 package opensource.builditbigger;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.Jokes;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_show_joke)
+    TextView tv_show_jokes;
+
+    Jokes joke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        joke = new Jokes();
+
+        tv_show_jokes.setText(joke.getJoke());
+    }
+
+    @OnClick(R.id.btn_joke)
+    void showJoke() {
+        Toast.makeText(this, joke.getJoke(), Toast.LENGTH_SHORT).show();
     }
 }
